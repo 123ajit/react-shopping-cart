3b1bc6606f9503d3e4f8a0e32a5fef7e72dc09ca
123ajit <ajit45288@gmail.com>
2021-09-03 10:21:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All right is reserved.</footer>
+    </div>
+  );
+}
